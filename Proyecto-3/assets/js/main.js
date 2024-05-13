3cb447b4a9dabcf5c61de6945cada6a74d0fa67f
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-05-13 17:34:03 -0600

    add commit

---
+++ Proyecto-3/assets/js/main.js
@@ -0,0 +1,9 @@
+let velocidad = 150;
+const limite = 60;
+
+
+if ( velocidad > limite){
+    console.log('Te has pasado de la velocidad permitida! vas a '+velocidad+'km/h y el limite es 60km/h');
+} else{
+        console.log('No te has pasado de la velocidad permitida!');
+    }
