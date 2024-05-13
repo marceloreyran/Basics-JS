f2b9151230de3c80a80918944120c607f9017081
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-05-13 16:45:54 -0600

    add commit

--- Proyecto-2/assets/main.js
+++ Proyecto-2/assets/main.js
@@ -1,11 +1,15 @@
-let marca = 'Audi',    
+let marcas = [
+    ' Audi ',
+    ' BMW ', 
+    ' Mercedes Benz ' 
+];
     velocidad = '250',
     stock = true,
-    modelos = ['A5','A8','S8','Q5'];
+    modelos = ['A5','A8','S8','Q5','X5','X7','CLA'];
 
 
 
-     let frase = `<p>Tenemos coches de la marca ${marca},
+     let frase = `<p>Tenemos coches de la marcas ${marcas},
      llegan a una velocidad maxima de ${velocidad}km/h,</p>
      <p>Tenemos los siguientes modelos en ${stock}:</p>
      `;
@@ -18,3 +22,5 @@ let marca = 'Audi',
 
     let caja = document.querySelector('#caja');
     caja.innerHTML = frase;
+
+
