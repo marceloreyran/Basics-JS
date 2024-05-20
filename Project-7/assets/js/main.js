1c340bc1ba3cadada19f96a90ce6b6479468e86c
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-05-20 17:42:35 -0600

    add commit

---
+++ Project-7/assets/js/main.js
@@ -0,0 +1,15 @@
+const actualYear = new Date().getFullYear();
+
+
+let year = 1990;
+let result = 0;
+
+do {
+    year = parseInt(prompt("En que año naciste?", year));
+
+} while (year < (actualYear - 150) || year > actualYear);
+
+result = actualYear - year;
+
+
+alert("Tienes "  +result+  " Años");
