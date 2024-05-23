4fb5ae72753f206d2224b8015f8025216fbdd1bb
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-05-23 15:57:45 -0600

    other commit

--- Project-10/assets/js/main.js
+++ Project-10/assets/js/main.js
@@ -8,3 +8,25 @@ function countAndUpperLetters(word){
 }
 
 alert(countAndUpperLetters(text));
+
+
+/*
+    Este código JavaScript solicita al usuario que ingrese una palabra o frase. Luego, la función countAndUpperLetters convierte esta entrada a mayúsculas y cuenta la cantidad de caracteres que tiene. Finalmente, se muestra una alerta con la palabra convertida a mayúsculas y la cantidad de caracteres que tiene.
+ // Solicita al usuario que ingrese una palabra o frase
+// Solicita al usuario que ingrese una palabra o frase
+let texto = prompt('Ingresa unas Palabras');
+
+// Define una función que toma una palabra o frase
+function contarYMayusculas(palabra){
+    // Convierte la palabra o frase a mayúsculas
+    let mayusculas = palabra.toUpperCase();
+    // Cuenta la cantidad de caracteres en la palabra o frase
+    let contador = palabra.length;
+
+    // Devuelve la palabra o frase en mayúsculas y la cantidad de caracteres
+    return 'La palabra ha sido: '+mayusculas+' y tiene '+contador+' letras';
+}
+
+// Muestra una alerta con la palabra o frase en mayúsculas y la cantidad de caracteres
+alert(contarYMayusculas(texto));
+*/
