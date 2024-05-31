7ef5ea507325d9f2907f1aa7ceca8ebb82816d24
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-05-31 17:32:28 -0600

    other commit

--- Project-14/assets/main/main.js
+++ Project-14/assets/main/main.js
@@ -1,1 +1,16 @@
-alert('Esta es una alerte');
+const sueldos = [1200, 2200, 3300, 4400, 5500, 6600, 11000];
+
+let sueldoUsuario = parseInt(prompt("Cual es tu sueldo?"));
+
+
+function buscarSueldos(salarios, miSueldo){
+
+    let busqueda = salarios.filter(salario => salario >= miSueldo);
+    let ordenar = busqueda.sort((a, b) => a - b);
+
+    console.log("Hay varios sueldos mayores a "+ miSueldo +", son estos");
+
+    for( let i in ordenar) console.log(ordenar[i]);
+}
+
+buscarSueldos(sueldos, sueldoUsuario);
